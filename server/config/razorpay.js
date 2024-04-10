d83da5efeef3fa5fb735f88a020e8cea98610860
Atul /maurya <103453238+atul-5242@users.noreply.github.com>
2024-04-11 00:53:25 +0530

    process.env chnages in file some place.

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,7 +1,7 @@
 const Razorpay = require("razorpay");
 
 exports.instance = new Razorpay({
-    key_id:"rzp_test_XToHNGFyYidB0m",
+    key_id:process.env.RAZORPAY_KEY_,
     key_secret:process.env.RAZORPAY_SECRET,
     
 });
