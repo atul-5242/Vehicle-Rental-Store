b318a3df78355376e0ab6d8cd313297f1086bf0c
Atul /maurya <103453238+atul-5242@users.noreply.github.com>
2024-04-11 03:21:20 +0530

    7th change for cores.

--- server/index.js
+++ server/index.js
@@ -24,13 +24,22 @@ database.connect();
 // middlewares addition:
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-    cors({
-        origin: ["https://vehical-rental-store.vercel.app", "https://vehical-rental-store-indol.vercel.app"],
-        credentials: true,
-    })
-)
+// app.use(
+//     cors({
+//         origin: ["https://vehical-rental-store.vercel.app", "https://vehical-rental-store-indol.vercel.app"],
+//         credentials: true,
+//     })
+// )
 
+
+// Allow requests from specific origins
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://vehical-rental-store-indol.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  });
 
 app.use(
     fileUpload({
